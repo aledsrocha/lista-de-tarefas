5a8e6adc0d3cd6a1eff65a6c424835f1213bb41d
Alessandro Rocha <aledsrocha@gmail.com>
2023-03-14 17:18:12 -0300

    criando conexao com db

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+
+    session_start();
+    $base = 'http://localhost/tarefas';
+
+    $db_name = 'listatarefas';
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+
+    $pdo = new PDO("mysql:dbname=$db_name;host=$db_host;", $db_user, $db_pass);
+ ?>
