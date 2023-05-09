64f0bd070fa3520b65133f084c3fd497eef5da3c
Alessandro Rocha <aledsrocha@gmail.com>
2023-05-09 17:24:38 -0300

    adicionando a interface ao sistema

--- index.php
+++ index.php
@@ -14,4 +14,33 @@
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="<?=$base;?>/assent/css/style.css">
+	<title>LISTA DE TAREFAS</title>
+</head>
+<body>
+	<h1>MINHA LISTA DE TAREFAS</h1><br><br><br>
+	<form method="post" action="cadastrar_action.php" class="formulario">
+		<label>Tarefa</label>
+		<input type="text" name="tarefas" class="barra" placeholder="digite sua tarefa">
+		<label>Autor</label>
+		<input type="text" name="name" class="barra" placeholder="nome do proprietario">
+		<label>status</label>
+		<select name="select">
+			<option></option>
+			<option>Pendente</option>
+			<option>Concluído</option>
+			
+		</select><br><br>
+
+		<input type="submit" name="" value="Cadastrar Tarefas" class="enviar">
+	</form>
+
+</body>
+</html>
+
 
